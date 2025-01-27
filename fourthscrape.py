79973abc5d8e6225eeca85c81eef88159a2de131
Jordan <jscheftic@spsi.com>
2025-01-27 13:23:09 -0600

    added polyneon logic

--- fourthscrape.py
+++ fourthscrape.py
@@ -43,7 +43,10 @@ def search_thread(thread_number):
         # Wait for the detail page to load
         paragraph_element = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located(
-                (By.CSS_SELECTOR, "#ctl00_ContentPlaceHolder_PageContent1_ProductDetail_pnlSingleItemDetail > div.desc > p:nth-child(7)")
+                # for Rayon threads
+                # (By.CSS_SELECTOR, "#ctl00_ContentPlaceHolder_PageContent1_ProductDetail_pnlSingleItemDetail > div.desc > p:nth-child(7)")
+                # for Polyneon Threads
+                (By.CSS_SELECTOR, "#ctl00_ContentPlaceHolder_PageContent1_ProductDetail_pnlSingleItemDetail > div.desc > p")
             )
         )
 
@@ -75,7 +78,8 @@ def search_thread(thread_number):
         driver.get("https://www.madeirausa.com/")
 
 # Input the thread numbers you want to search for
-thread_numbers = ['1000', '1001', '1002', '1005', '1006', '1010']
+thread_numbers =  [1905, 1906, 1907, 1908, 1909, 1910, 1911, 1915, 1917, 1918, 1919, 1920, 1921, 1922, 1923, 1924, 1925, 1926, 1927, 1928, 1929, 1930, 1931, 1932, 1933, 1934, 1935, 1936, 1937, 1938, 1939, 1940, 1941, 1942, 1943, 1944, 1945, 1946, 1947, 1948, 1949, 1950, 1951, 1952, 1953, 1954, 1955, 1956, 1957, 1958, 1959, 1960, 1961, 1962, 1963, 1964, 1965, 1966, 1967, 1968, 1969, 1970, 1971, 1972, 1973, 1974, 1975, 1976, 1977, 1978, 1979, 1980, 1981, 1982, 1983, 1984, 1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+
 results = {}
 
 # Loop through each thread number and collect results
